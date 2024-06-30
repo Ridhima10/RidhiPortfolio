c5a6e1a6e47ca8454cf563bce5bab9db630561cc
Ridhima Jain <85826117+Ridhima10@users.noreply.github.com>
2024-06-30 14:48:34 +0530

    Initial commit

---
+++ utils/data/educations.js
@@ -0,0 +1,26 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Masters Degree",
+    duration: "2023 - Present",
+    institution: "Chandigarh group of colleges, Landran",
+  },
+  {
+    id: 2,
+    title: "Bachelor Degree",
+    duration: "2019-2022",
+    institution: "Cluster University of Jammu",
+  },
+  {
+    id: 3,
+    title: "Higher Secondary",
+    duration: "2017 - 2019",
+    institution: "Oriental Academy",
+  },
+  {
+    id: 4,
+    title: "Secondary School",
+    duration: "- 2017",
+    institution: "Kendriya Vidyalaya No.2 Jammu Cantt",
+  }
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "RIDHIMA JAIN",
+  profile: '/profile.jpeg',
+  designation: "Web Developer",
+  description: "My name is RIDHIMA. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world.  I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'ridhimajain862@gmail.com',
+  phone: '+919622037097',
+  address: 'Golden Homes Kharar-Landran RD, sector 127, Punjab, 140301, India',
+  github: 'https://github.com/Ridhima10',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/ridhima-jain-96829a212/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "Ridhima10",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
